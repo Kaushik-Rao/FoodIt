96657d418ce50bc51f96cee4921480b498dcbba3
Kaushik-Rao <blikekaushikrao@gmail.com>
2024-06-19 21:03:17 +0530

    first commit

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,22 @@
+
+import { prisma } from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+
+//FETCH ALL PRODUCTS
+export const GET = async () => {
+  try {
+    const products = await prisma.product.findMany();
+    return new NextResponse(JSON.stringify(products), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("Hello", { status: 200 });
+};
